e05ea6d0c2dacfbf3aa813cb277f5e7fa300f15e
“[Ayesha <amjadayesha222@gmail.com>
2023-10-20 10:59:34 -0700

    Google Authentication of Safe app
    Sign up with Google Account module implemented

--- safe/lib/pages/sign_up_screen.dart
+++ safe/lib/pages/sign_up_screen.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({Key? key});
+
+  googleLogin() async {
+    print("Google Login function");
+    GoogleSignIn _googleSignIn = GoogleSignIn( 
+      clientId: '36466737574-dut5h5ale50o89sn861i1hrpkcklk0bk.apps.googleusercontent.com',
+    );
+    try {
+    var result = await _googleSignIn.signIn();
+    print(result);
+  } catch (error) {
+    print(error);
+  }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,157 +25,185 @@ class SignupScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: const Color(0XFFEEE9DA), // Background color
-      body: SingleChildScrollView(
-        child: Center(
-          // Center the entire content
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 0, left: 0),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Image.asset(
-                    'assets/upr_corner.png',
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
+      body: Center(
+        // Center the entire content
+        child: Stack(
+          children: <Widget>[
+            // Image in top-left corner
+            Positioned(
+              left: -15,
+              top: 0,
+              child: Image.asset(
+                'assets/upr_corner.png',
+                width: 100,
+                height: 100,
               ),
+            ),
+            Positioned(
+              right: -15,
+              top: -30,
+              child: Image.asset(
+                'assets/safe_logo.png',
+                width: 150,
+                height: 150,
+              ),
+            ),
+            Positioned(
+              right: -15,
+              bottom: 0,
+              child: Image.asset(
+                'assets/btm_corner.png',
+                width: 100,
+                height: 100,
+              ),
+            ),
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Center horizontally
+              children: <Widget>[
                 Padding(
-                padding: const EdgeInsets.only(top: 0, right: 0),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: Image.asset(
-                    'assets/safe_logo.png',
-                    width: 100,
-                    height: 100,
-                  ),
-                ),
-              ),
-             
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start, // Center vertically
-                crossAxisAlignment: CrossAxisAlignment.start, // Center horizontally
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(6.0, 1.0, 0, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center, // Center horizontally
-                      children: <Widget>[
-                        const Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 0, 0, 0), // Text color
-                          ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.center, // Center horizontally
+                    children: <Widget>[
+                      const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0), // Text color
                         ),
-                        const SizedBox(height: 20),
-                        Image.asset(
+                      ),
+                      const SizedBox(height: 20),
+                      Center(
+                        child: Image.asset(
                           'assets/signup.png',
                           width: 100,
                           height: 100,
                         ),
-                        const SizedBox(height: 10),
-                        const TextFieldWithIcon(Icons.account_circle, 'Name'),
-                        const SizedBox(height: 10),
-                        const TextFieldWithIcon(Icons.phone, 'Phone Number'),
-                        const SizedBox(height: 10),
-                        const TextFieldWithIcon(FontAwesomeIcons.addressBook, 'Address'),
-                        const SizedBox(height: 10),
-                        const TextFieldWithIcon(Icons.email, 'Email'),
-                        const SizedBox(height: 10),
-                        const TextFieldWithIcon(Icons.lock, 'Password'),
-                        const SizedBox(height: 10),
-                        ElevatedButton(
-                          onPressed: () {
-                            // Navigate to "Sign In" page
-                            Navigator.pushNamed(context, '/login');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            primary: const Color(0XFF6096B4), // Change button color
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20), // No border radius
-                            ),
+                      ),
+                      const SizedBox(height: 10),
+                      const TextFieldWithIcon(Icons.account_circle, 'Name'),
+                      const SizedBox(height: 10),
+                       const TextFieldWithIcon(Icons.phone, 'Phone Number'),
+                      const SizedBox(height: 10),
+                       const TextFieldWithIcon(FontAwesomeIcons.addressBook, 'Address'),
+                      const SizedBox(height: 10),
+                      const TextFieldWithIcon(Icons.email, 'Email'),
+                      const SizedBox(height: 10),
+                      const TextFieldWithIcon(Icons.lock, 'Password'),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        onPressed: () {
+                          
+                          // Navigate to "Sign In" page
+                          Navigator.pushNamed(context, '/login');
+                        },
+                          
+                        style: ElevatedButton.styleFrom(
+                          primary:
+                              const Color(0XFF6096B4), // Change button color
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(20), // No border radius
                           ),
-                          child: SizedBox(
-                            width: screenSize.width * 0.8, // Adjust button width based on screen width
-                            child: const Align(
-                              alignment: Alignment.center,
-                              child: Text(
-                                "Sign Up",
-                                style: TextStyle(fontSize: 18),
-                              ),
+                        ),
+                        child: SizedBox(
+                          width: screenSize.width *
+                              0.8, // Adjust button width based on screen width
+                          child: const Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(fontSize: 18),
                             ),
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            const Text(
-                              "Already have an account? ",
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            "Already have an account? ",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              // Navigate to the login page
+                              Navigator.pushNamed(context, '/login');
+                            },
+                            child: const Text(
+                              "Sign In",
                               style: TextStyle(
                                 fontSize: 16,
+                                fontWeight: FontWeight.bold,
                                 color: Colors.black,
                               ),
                             ),
-                            GestureDetector(
-                              onTap: () {
-                                // Navigate to the login page
-                                Navigator.pushNamed(context, '/login');
-                              },
-                              child: const Text(
-                                "Sign In",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ],
+                          ),
+                        ],
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          googleLogin(); // google sign up function----------------------------------------------
+                          // Navigate to "Sign In" page
+                          Navigator.pushNamed(context, '/menu');
+                        },
+                          
+                        style: ElevatedButton.styleFrom(
+                          primary:
+                              const Color(0XFF6096B4), // Change button color
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(20), // No border radius
+                          ),
                         ),
-                        const SizedBox(height: 20),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            RoundedIconButton(
-                              Icons.facebook,
-                              const Color(0XFF6096B4),
+                        child: SizedBox(
+                          width: screenSize.width *
+                              0.8, // Adjust button width based on screen width
+                          child: const Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Sign Up With Google",
+                              style: TextStyle(fontSize: 18),
                             ),
-                            const SizedBox(width: 20),
-                            RoundedIconButton(
-                              FontAwesomeIcons.google,
-                              const Color(0XFF6096B4),
-                            ),
-                            const SizedBox(width: 20),
-                            RoundedIconButton(
-                              FontAwesomeIcons.twitter,
-                              const Color(0XFF6096B4),
-                            ),
-                          ],
+                          ),
                         ),
-                        SizedBox(height: MediaQuery.of(context).viewInsets.bottom),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 0, right: 0),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Image.asset(
-                    'assets/btm_corner.png',
-                    width: 100,
-                    height: 100,
+                      ),
+                      // const SizedBox(height: 20),
+                      // const Row(
+                      //   mainAxisAlignment: MainAxisAlignment.center,
+                      //   children: <Widget>[
+                      //     RoundedIconButton(
+                      //       Icons.facebook,
+                      //       const Color(0XFF6096B4),
+                      //     ),
+                      //     SizedBox(width: 20),
+                      //     RoundedIconButton(
+                      //       FontAwesomeIcons.google,
+                      //       const Color(0XFF6096B4),
+                            
+                      //     ),
+                      //     SizedBox(width: 20),
+                      //     RoundedIconButton(
+                      //       FontAwesomeIcons.twitter,
+                      //       const Color(0XFF6096B4),
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
@@ -177,7 +220,8 @@ class TextFieldWithIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     return Container(
-      width: screenSize.width * 0.9, // Adjust the field width based on screen width
+      width: screenSize.width *
+          0.9, // Adjust the field width based on screen width
       decoration: BoxDecoration(
         color: const Color.fromARGB(184, 184, 210, 216),
         borderRadius: BorderRadius.circular(10),
@@ -212,8 +256,8 @@ class RoundedIconButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 40,
-      height: 40,
+      width: 50,
+      height: 50,
       decoration: BoxDecoration(
         color: color,
         shape: BoxShape.circle,
