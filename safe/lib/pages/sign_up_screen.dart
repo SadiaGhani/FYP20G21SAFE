2157568addd0f6545794d5a91ef1ff961bcf0435
Esha Tur Razia <esharao937@gmail.com>
2024-02-15 00:52:20 -0800

    updated sign up added terms and condition checkbox
    updated sign up added terms and condition checkbox

--- safe/lib/pages/sign_up_screen.dart
+++ safe/lib/pages/sign_up_screen.dart
@@ -1,111 +1,128 @@
 //import 'dart:js';
-import 'package:safe/pages/main_menu/menu_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 //import 'package:google_sign_in/google_sign_in.dart';
 import 'package:safe/pages/google_signin_api.dart';
-
+import 'package:safe/pages/main_menu/menu_screen.dart';
 
 class SignupScreen extends StatelessWidget {
-  
-
-  
-
   Future signIn(context) async {
     print("i am in funtionnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
     final user = await GoogleSignInApi.login();
     print(user);
-    if(user == null){
+    if (user == null) {
       print("Erorrrrrrrrrrrrrrrrr");
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Sign Up failed")));
-    }else{
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Sign Up failed")));
+    } else {
       print("sucessssssssssssss");
       Navigator.of(context).pushReplacement(MaterialPageRoute(
         builder: (context) => MenuPage(),
-        ));
+      ));
     }
-  // print("Google Login functioneeeeeeeee)eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-  // GoogleSignIn _googleSignIn = GoogleSignIn(
-  //   serverClientId: '36466737574-dut5h5ale50o89sn861i1hrpkcklk0bk.apps.googleusercontent.com',
-    
-  // );
+    // print("Google Login functioneeeeeeeee)eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+    // GoogleSignIn _googleSignIn = GoogleSignIn(
+    //   serverClientId: '36466737574-dut5h5ale50o89sn861i1hrpkcklk0bk.apps.googleusercontent.com',
 
-  // try {
-  //   var result = await _googleSignIn.signIn();
+    // );
 
-  //   if (result != null) {
-  //     // navigator();
-  //     print(" i am in trueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-  //     print(result);
-  //     return true;
-  //   } else {
-  //     // User canceled the Google sign-in
-  //     return false;
-  //   }
-  // } catch (error, stackTrace) {
-  // print(" i am in errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
-  // print('Error signing in: $error');
-  // print('Stack trace: $stackTrace');
-  // return false;
-  // }
-}
+    // try {
+    //   var result = await _googleSignIn.signIn();
 
+    //   if (result != null) {
+    //     // navigator();
+    //     print(" i am in trueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+    //     print(result);
+    //     return true;
+    //   } else {
+    //     // User canceled the Google sign-in
+    //     return false;
+    //   }
+    // } catch (error, stackTrace) {
+    // print(" i am in errrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+    // print('Error signing in: $error');
+    // print('Stack trace: $stackTrace');
+    // return false;
+    // }
+  }
 
-
-void signUpWithEmailAndPassword(String email, String password, BuildContext context) {
-  FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: password)
-      .then((value) {
-        // Account creation successful
-        _showConfirmationDialog(context);
-      })
-      .catchError((error) {
-        // Account creation failed
-        print("Error: ${error.toString()}");
-        // You may want to display an error message to the user
-      });
-}
+  void signUpWithEmailAndPassword(
+      String email, String password, BuildContext context) {
+    FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password)
+        .then((value) {
+      // Account creation successful
+      _showConfirmationDialog(context);
+    }).catchError((error) {
+      // Account creation failed
+      print("Error: ${error.toString()}");
+      // You may want to display an error message to the user
+    });
+  }
 
   void _showTermsAndConditionsDialog(BuildContext context) {
+    bool checkedValue = false; // Initially unchecked
+
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Terms and Conditions'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text(
-                    'Here are the terms and conditions of the app. We take your data security seriously. The SAFE app employs encryption and other security measures to protect your personal information and data. '),
-                Text(
-                    ' The remote data wipe feature allows you to erase data from your device remotely. Use this feature with caution, as it will permanently delete all data on your device.'),
-                Text(
-                    'The SAFE app is intended for personal use only. You may not use the app for any illegal or unauthorized purpose.'),
-                Text(
-                    'We reserve the right to modify or update these terms and conditions at any time. Any changes will be effective immediately upon posting.'),
+        return StatefulBuilder(
+          // Wrap AlertDialog with StatefulBuilder
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text('Terms and Conditions'),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    Text(
+                        'Here are the terms and conditions of the app. We take your data security seriously. The SAFE app employs encryption and other security measures to protect your personal information and data. '),
+                    Text(
+                        ' The remote data wipe feature allows you to erase data from your device remotely. Use this feature with caution, as it will permanently delete all data on your device.'),
+                    Text(
+                        'The SAFE app is intended for personal use only. You may not use the app for any illegal or unauthorized purpose.'),
+                    Text(
+                        'We reserve the right to modify or update these terms and conditions at any time. Any changes will be effective immediately upon posting.'),
+                    CheckboxListTile(
+                      title: Text("I agree to the terms and conditions"),
+                      value: checkedValue,
+                      onChanged: (newValue) {
+                        setState(() {
+                          // Update the state of the checkbox
+                          checkedValue = newValue!;
+                        });
+                      },
+                      controlAffinity: ListTileControlAffinity.leading,
+                    ),
+                  ],
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Close the dialog
+                  },
+                  child: Text('Cancel'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    if (checkedValue == true) {
+                      Navigator.pop(context); // Close the dialog
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MenuPage(),
+                        ),
+                      );
+                    } else {
+                      // Show a message or handle the case when terms are not accepted
+                    }
+                  },
+                  child: Text('Register'),
+                ),
               ],
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context); // Close the dialog
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context); // Close the dialog
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MenuPage(),
-                  ),
-                );
-              },
-              child: Text('Agree'),
-            ),
-          ],
+            );
+          },
         );
       },
     );
@@ -133,10 +150,7 @@ void signUpWithEmailAndPassword(String email, String password, BuildContext cont
     );
   }
 
-
-
-
- void showAlertDialog(BuildContext context, String title, String message) {
+  void showAlertDialog(BuildContext context, String title, String message) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -157,42 +171,39 @@ void signUpWithEmailAndPassword(String email, String password, BuildContext cont
   }
 
   bool isValidPhoneNumber(String phoneNumber) {
-  // Implement your phone number validation logic. This is a basic example.
-  // You might want to use regular expressions or other methods for more robust validation.
-  return phoneNumber.length == 11 && phoneNumber.startsWith('0');
+    // Implement your phone number validation logic. This is a basic example.
+    // You might want to use regular expressions or other methods for more robust validation.
+    return phoneNumber.length == 11 && phoneNumber.startsWith('0');
   }
 
-
   bool isValidPassword(String password) {
-  // Basic check for a minimum length of the password. You can customize this based on your requirements.
-  return password.length >= 6;
+    // Basic check for a minimum length of the password. You can customize this based on your requirements.
+    return password.length >= 6;
   }
 
   bool isValidEmail(String email) {
-  // Basic check for a valid email address using a simple regular expression.
-  // You might want to use a more sophisticated email validation library.
-  final emailRegExp = RegExp(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
-  return emailRegExp.hasMatch(email);
+    // Basic check for a valid email address using a simple regular expression.
+    // You might want to use a more sophisticated email validation library.
+    final emailRegExp =
+        RegExp(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$');
+    return emailRegExp.hasMatch(email);
   }
 
   bool isValidName(String value) {
-  // Basic check for alphabets only. You can customize this based on your requirements.
-  final RegExp alphaRegExp = RegExp(r'^[a-zA-Z ]+$');
-  return alphaRegExp.hasMatch(value);
-}
-
-
-
+    // Basic check for alphabets only. You can customize this based on your requirements.
+    final RegExp alphaRegExp = RegExp(r'^[a-zA-Z ]+$');
+    return alphaRegExp.hasMatch(value);
+  }
 
   final TextEditingController nameController = TextEditingController();
   final TextEditingController phoneNumberController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
-
-@override
+  @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
 
@@ -258,63 +269,76 @@ void signUpWithEmailAndPassword(String email, String password, BuildContext cont
                           height: 100,
                         ),
                       ),
-                      
-                      TextFieldWithIcon(Icons.account_circle, 'Name', nameController),
+
+                      TextFieldWithIcon(
+                          Icons.account_circle, 'Name', nameController),
                       const SizedBox(height: 10),
-                      TextFieldWithIcon(Icons.phone, 'Phone Number', phoneNumberController),
+                      TextFieldWithIcon(
+                          Icons.phone, 'Phone Number', phoneNumberController),
                       const SizedBox(height: 10),
-                      TextFieldWithIcon(FontAwesomeIcons.addressBook, 'Address', addressController),
+                      TextFieldWithIcon(FontAwesomeIcons.addressBook, 'Address',
+                          addressController),
                       const SizedBox(height: 10),
                       TextFieldWithIcon(Icons.email, 'Email', emailController),
                       const SizedBox(height: 10),
-                      TextFieldWithIcon(Icons.lock, 'Password', passwordController, obscureText: true),
+                      TextFieldWithIcon(
+                          Icons.lock, 'Password', passwordController,
+                          obscureText: true),
                       const SizedBox(height: 10),
-                      TextFieldWithIcon(Icons.lock, 'Confirm Password', confirmPasswordController, obscureText: true),
+                      TextFieldWithIcon(Icons.lock, 'Confirm Password',
+                          confirmPasswordController,
+                          obscureText: true),
 
                       ElevatedButton(
-                        
                         onPressed: () {
                           String name = nameController.text;
                           String phoneNumber = phoneNumberController.text;
                           String address = addressController.text;
                           String email = emailController.text;
                           String password = passwordController.text;
-                          String confirmPassword = confirmPasswordController.text;
-                          
-                          if (name.isEmpty || phoneNumber.isEmpty || address.isEmpty || email.isEmpty || password.isEmpty || confirmPassword.isEmpty) {
+                          String confirmPassword =
+                              confirmPasswordController.text;
+
+                          if (name.isEmpty ||
+                              phoneNumber.isEmpty ||
+                              address.isEmpty ||
+                              email.isEmpty ||
+                              password.isEmpty ||
+                              confirmPassword.isEmpty) {
                             String title = "All Feilds are required";
-                            String msg = "Please fill out all the asked information";
-                            showAlertDialog(context,title,msg);
-                            } else if (!isValidName(name)) {
+                            String msg =
+                                "Please fill out all the asked information";
+                            showAlertDialog(context, title, msg);
+                          } else if (!isValidName(name)) {
                             String title = "Invalid Name";
-                            String msg = "Your name should contains only alphabets";
-                            showAlertDialog(context,title,msg);
-                            }
-                            else if (!isValidEmail(email)) {
+                            String msg =
+                                "Your name should contains only alphabets";
+                            showAlertDialog(context, title, msg);
+                          } else if (!isValidEmail(email)) {
                             String title = "Invalid Email";
                             String msg = "Please enter a Valid Email Address";
-                            showAlertDialog(context,title,msg);
-                            }  
-                            else if (!isValidPhoneNumber(phoneNumber)) {
+                            showAlertDialog(context, title, msg);
+                          } else if (!isValidPhoneNumber(phoneNumber)) {
                             String title = "Invalid Phone Number";
-                            String msg = "Please Enter the Valid phone Number of 11 digits";
-                            showAlertDialog(context,title,msg);
-                            } else if (!isValidPassword(password)) {
+                            String msg =
+                                "Please Enter the Valid phone Number of 11 digits";
+                            showAlertDialog(context, title, msg);
+                          } else if (!isValidPassword(password)) {
                             String title = "Password Error";
-                            String msg = "You Password should be at least6 character long and contains atleast 1 Special Character";
-                            showAlertDialog(context,title,msg);
-                            } 
-                            else if (password != confirmPassword) {
+                            String msg =
+                                "You Password should be at least6 character long and contains atleast 1 Special Character";
+                            showAlertDialog(context, title, msg);
+                          } else if (password != confirmPassword) {
                             String title = "Password Mismatch error";
-                            String msg = "Your Password and Confirm Password did not Match";
-                            showAlertDialog(context,title,msg);
-                            } 
-                            else {
+                            String msg =
+                                "Your Password and Confirm Password did not Match";
+                            showAlertDialog(context, title, msg);
+                          } else {
                             // All data is valid, proceed with sign-up
-                            signUpWithEmailAndPassword(email, password, context);
-                            }
+                            signUpWithEmailAndPassword(
+                                email, password, context);
+                          }
                         },
-                          
                         style: ElevatedButton.styleFrom(
                           primary:
                               const Color(0XFF6096B4), // Change button color
@@ -368,12 +392,11 @@ void signUpWithEmailAndPassword(String email, String password, BuildContext cont
 
                           signIn(context);
 
-                        //   if (loginSuccess) {
-                        //   // Navigate to "Sign In" page only if Google login was successful
-                        //   Navigator.pushNamed(context, '/menu');
-                        //  }
+                          //   if (loginSuccess) {
+                          //   // Navigate to "Sign In" page only if Google login was successful
+                          //   Navigator.pushNamed(context, '/menu');
+                          //  }
                         },
-                          
                         style: ElevatedButton.styleFrom(
                           primary:
                               const Color(0XFF6096B4), // Change button color
@@ -406,7 +429,7 @@ void signUpWithEmailAndPassword(String email, String password, BuildContext cont
                       //     RoundedIconButton(
                       //       FontAwesomeIcons.google,
                       //       const Color(0XFF6096B4),
-                            
+
                       //     ),
                       //     SizedBox(width: 20),
                       //     RoundedIconButton(
@@ -433,7 +456,8 @@ class TextFieldWithIcon extends StatelessWidget {
   final TextEditingController controller;
   final bool obscureText;
 
-  const TextFieldWithIcon(this.icon, this.hintText, this.controller, {this.obscureText = false});
+  const TextFieldWithIcon(this.icon, this.hintText, this.controller,
+      {this.obscureText = false});
 
   @override
   Widget build(BuildContext context) {
